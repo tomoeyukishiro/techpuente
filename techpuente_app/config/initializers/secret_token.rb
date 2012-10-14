2c5efb3a9fceea8848b92ba9842ffd13b0f7a801
Eric Ren <rencire@gmail.com>
2012-10-13 20:23:13 -0700

    generated skeleton rails project

---
+++ techpuente_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TechpuenteApp::Application.config.secret_token = '0eb31c1373c4a12a4b40960898e55a791812c564ac0da49212fc1c22a458daba03d517ca267d4f8c0a2ed0df0fa64177794bb1f08bd97b8e4cdeb29839aae7d0'
