0c9105e7fb5b0b74a041c8a40d312bf6ec2509ec
RicardoZeballos <z.rick3@berkeley.edu>
2012-11-04 14:10:11 -0800

    Added the some basic flash capabilites, added log out button. Need to add logout button path.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,19 +9,19 @@ class HomeController < ApplicationController
   end
 
 	def create  
-    user = User.find_by_email(params[:email])  
-  	if user && user.authenticate(params[:password])  
-    	session[:user_id] = user.id  
-    	redirect_to root_url, :notice => "Logged in!"  
-  	else
-			flash.alert = "Wrong Password or Email"    
-    	redirect_to "/"
+		user = User.find_by_email(params[:email])  
+		if user && user.authenticate(params[:password])  
+	   	session[:user_id] = user.id  
+	   	redirect_to root_url, flash[:notice] = "Your logged in!"   
+	 	else
+			flash[:notice] = "Wrong Password or Email"    
+		 	redirect_to "/"
   	end  
   end  
   
   def destroy  
     session[:user_id] = nil  
-    redirect_to root_url, :notice => "Logged out!"  
+    redirect_to root_url, flash[:notice] = "You logged out!"  
   end
   
 end
