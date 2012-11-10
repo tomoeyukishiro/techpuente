71d85564802f9dd8d64c15b4f16b645cc5f17290
RicardoZeballos <z.rick3@berkeley.edu>
2012-11-09 22:02:42 -0800

    Cleaned up the Admin authorization portion and added the join table for users and lessons.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,10 @@ class UsersController < ApplicationController
 	end
 
 	def create
+		#Mass Assigment Vunerble
+		#checked_admin = params[:admin]
 		@user = User.new(params[:user])
+		puts params[:user]
 		if @user.save
 			redirect_to lessons_path, :notice => "Signed Up!"
 		else
