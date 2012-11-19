d2feae55db7d6bf526ca4f3f32edb8f3e436062f
Shiv Pande <skpande@gmail.com>
2012-11-19 02:36:06 -0800

    working on rich editor tests

--- features/step_definitions/rich_text_steps.rb
+++ features/step_definitions/rich_text_steps.rb
@@ -1,13 +1,13 @@
-# Then /I should see the rich text box/ do
-# end
-# 
-# When /I append (.*) to the (.*)/ do |a, b|
-# end
-# 
-# When /I embed (.*) to the (.*)/ do |a, b|
-# end
-# 
-# Then /I should see a Youtube video/ do
-# end
-# 
-# 
+ Then /I should see the rich text box/ do
+	
+ end
+ 
+ When /I append (.*) in bold to the (.*)/ do |a, b|
+ end
+ 
+ When /I embed (.*) to the (.*)/ do |a, b|
+ end
+ 
+ Then /I should see a Youtube video/ do
+ end
+
